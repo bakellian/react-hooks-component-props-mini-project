7fb3146370ddc603ee90d705888f8a10eea26be5
Bridget Akellian <akellianbv@gmail.com>
2022-04-24 14:26:49 -0400

    completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,21 @@
 import react from "react";
 import App from "./App";
+import Article from "./Article";
 
-function ArticleList() {
+function ArticleList(props) {
+    const postArr = props.blogPosts.map((post) => {
+        return <Article 
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+            />
+    })
     return(
-        //hi
+       <main>
+           {postArr}
+           {/* return an array of article components */}
+       </main>
     )
 }
 
